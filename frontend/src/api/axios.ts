905f2436ae44b4a9b056bed03f690ef0eba3686b
barathsuresh <barathsuresh3@gmail.com>
2026-05-12 00:30:45 -0700

    feat: add axios instance with auth and 401 interceptors

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { getToken, clearToken } from '@/lib/token'
+import { navigateTo } from '@/lib/navigate'
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://127.0.0.1:8080',
+})
+
+instance.interceptors.request.use((config) => {
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      clearToken()
+      navigateTo('/login')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default instance
